2e3a40350f2522ba33854787c3943f1fe929ecf1
harris maung <harrismaung3@gmail.com>
2021-12-17 14:20:26 +0800

    updates

--- server/index.js
+++ server/index.js
@@ -12,12 +12,13 @@ app.use(express.json({limit: "30mb" , extended: true}));
 app.use(express.urlencoded({limit: "30mb" , extended: true}));
 app.use(cors());
 app.use('/posts', postRoutes);
+const CONNECTION_URL = 'mongodb+srv://harrismaung:harrismaung3@cluster0.b0taf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 app.get('/', (req,res)=> {
     res.send("Hello to Fakestagram API");
 })
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
