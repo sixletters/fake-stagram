0b37b77c72b573bef7fb603646927bd5ecff2797
harris maung <harrismaung3@gmail.com>
2021-12-17 14:13:42 +0800

    Further updates

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,5 @@
 import * as api from '../api';
+import {FETCH_ALL,CREATE, UPDATE, DELETE, LIKE} from '../constants/actionTypes'
 
 //action creators
 // action is an object that has the type and the payload
@@ -6,7 +7,7 @@ export const getPosts = () => async (dispatch) => {
 
     try{
         const { data } = await api.fetchPosts ()
-        dispatch({type : 'FETCH_ALL', payload: data});
+        dispatch({type : FETCH_ALL, payload: data});
     }catch(error){
             console.log(error.message)
     }
@@ -15,7 +16,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async(dispatch) => {
     try{
         const { data } = await api.createPost(post);
-        dispatch({type: 'CREATE', payload:data})
+        dispatch({type: CREATE, payload:data})
     }catch(error){
         console.log(error.message)
     }
@@ -24,7 +25,7 @@ export const createPost = (post) => async(dispatch) => {
 export const updatePost = (id,post) => async(dispatch) => {
     try{
         const {data} = await api.updatePost(id,post);
-        dispatch({type: 'UPDATE', payload: data});
+        dispatch({type:  UPDATE, payload: data});
     }catch(error){
         console.log(error.message)
     }
@@ -33,7 +34,7 @@ export const updatePost = (id,post) => async(dispatch) => {
 export const deletePost = (id) => async(dispatch) => {
     try{
         await api.deletePost(id)
-        dispatch({type: 'DELETE', payload: id})
+        dispatch({type: DELETE, payload: id})
     }catch(error){
         console.log(error)
     }
@@ -42,7 +43,7 @@ export const deletePost = (id) => async(dispatch) => {
 export const likePost = (id) => async(dispatch) => {
     try{
         const {data} = await api.likePost(id);
-        dispatch({type: 'LIKE', payload: data});
+        dispatch({type: LIKE, payload: data});
     }catch(error){
         console.log(error);
     }
