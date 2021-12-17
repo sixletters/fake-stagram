2d5141eac1d038804c3c79a6ceb129068eef38cc
harris maung <harrismaung3@gmail.com>
2021-12-17 14:21:33 +0800

    bug fixes

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js'
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
 const app = express();
-dotenv.config();
+// dotenv.config();
 
 app.use(express.json({limit: "30mb" , extended: true}));
 app.use(express.urlencoded({limit: "30mb" , extended: true}));
