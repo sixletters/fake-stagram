9b61030c6a1e203a2df361736f16734dcf3c248e
harris maung <harrismaung3@gmail.com>
2021-12-17 14:44:10 +0800

    updates

--- server/index.js
+++ server/index.js
@@ -6,19 +6,18 @@ import postRoutes from './routes/posts.js'
 import dotenv from 'dotenv';
 
 const app = express();
-// dotenv.config();
+dotenv.config();
 
 app.use(express.json({limit: "30mb" , extended: true}));
 app.use(express.urlencoded({limit: "30mb" , extended: true}));
 app.use(cors());
 app.use('/posts', postRoutes);
-const CONNECTION_URL = 'mongodb+srv://harrismaung:harrismaung3@cluster0.b0taf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 app.get('/', (req,res)=> {
     res.send("Hello to Fakestagram API");
 })
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
